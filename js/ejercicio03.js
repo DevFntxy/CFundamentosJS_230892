ef0b5cd5037de4343a02859c9b968daefd1b2c1d
Derek Sesni Carreño <230892@utxicotepec.edu.mx>
2024-09-19 17:13:38 -0600

    Practica 5
    Se agrego ejercicio03.js con el tipo de dato: objeto

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -3,13 +3,32 @@
 console.warn ("Objetos")
 
 const producto = {
-    nombre: "Tablet", //Para declarar una variable se ocupa : en vez de = y , al final de cada variable
-    precio : 300,
-    disponible : true
+
+    nombre: "Laptop", //Para declarar una variable se ocupa : en vez de = y , al final de cada variable
+    marca : "Huawei",
+    modelo : "D16",
+    costoCompra : 3000.12,
+    costoVenta: 3500,
+    disponible : true,
+    SKU: Symbol("S3SN1"),
+    colores: ["Gris","Gris claro"]
 }
 
 console.log(producto)
 console.table(producto) // Imprimir las propiedades en una tabla
+
+//Accder a las propiedades de un objeto
+
+console.warn("Leyendo las propiedades de un objeto y sus tipo de datos \n")
+
+console.log( `Nombre del producto: ${producto.nombre} que es del tipo ${typeof(producto.nombre)}
+ Marca del producto: ${producto.marcae} que es del tipo ${typeof(producto.marca)} 
+  Modelo del producto: ${producto.producto} que es del tipo ${typeof(producto.producto)} 
+  Costo de compra del producto: ${producto.costoCompra} que es del tipo ${typeof(producto.costoCompra)} 
+  Costo de venta  del producto: ${producto.costoVenta} que es del tipo ${typeof(producto.costoVenta)} 
+  Colores del producto: ${producto.colores} que es del tipo ${typeof(producto.colores)}` )
+
+
 
 console.log(producto.nombre) //Solo imprime una propiedad
 
